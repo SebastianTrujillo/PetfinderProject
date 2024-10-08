9ec054bf36e40e52789483436f274865800ad35b
Sebastian Rodriguez <jsrodriguez.user@gmail.com>
2024-10-08 12:39:35 -0500

    Update environment.prod.ts
    remove secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 	base_url_api: '/v2',
 	client_credentials: {
 		grant_type:"client_credentials",
-		client_id:"94my9CWeSYcNKTS59OmAEpKGYkL6Gg4woYCsbBiD1g0UmrN6M2",
-		client_secret:"1fiw8uQqJ41BmSZb41t0JqzUmoHAYCUS7g1Twxpm"
+		client_id:"",
+		client_secret:""
 	},
 	photo_coming_soon: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/48959574/1/?bust=1599092439&width=600"
 };
